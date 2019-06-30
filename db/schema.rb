869665e1b887455db94925c65c7c279e85455840
bl0rch1d <temaflash96@gmail.com>
2019-06-30 20:21:58 +0300

    Added Author, Book, Category, Material models with associations and validations

---
+++ db/schema.rb
@@ -0,0 +1,67 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_06_30_160806) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "authors_books", id: false, force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id", "book_id"], name: "index_authors_books_on_author_id_and_book_id"
+    t.index ["book_id", "author_id"], name: "index_authors_books_on_book_id_and_author_id"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price", precision: 10, scale: 2
+    t.integer "quantity"
+    t.string "dimensions"
+    t.integer "year"
+    t.string "materials"
+    t.string "image"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_books_on_category_id"
+  end
+
+  create_table "books_materials", id: false, force: :cascade do |t|
+    t.bigint "material_id", null: false
+    t.bigint "book_id", null: false
+    t.index ["book_id", "material_id"], name: "index_books_materials_on_book_id_and_material_id"
+    t.index ["material_id", "book_id"], name: "index_books_materials_on_material_id_and_book_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.integer "books_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "materials", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+CATEGORIES = ['Mobile development', 'Photo', 'Web design', 'Web development']
+MATERIALS = ['glossy paper', 'hardcover', 'soft paper', 'cardboard']
+LIMIT = 20
+BOOK_DIMENSIONS_RANGE = (1.0..10.0).freeze
+
+CATEGORIES.each do |category|
+  Category.create!(title: category)
+end
+
+MATERIALS.each do |material|
+  Material.create!(name: material)
+end
+
+LIMIT.times do
+  Author.create! do |author|
+    author.first_name = FFaker::Name.first_name
+    author.last_name = FFaker::Name.last_name
+  end
+
+  Book.create! do |book|
+    book.title = FFaker::Book.title
+    book.description = FFaker::Book.description
+    book.price = rand(5.0..200.00)
+    book.quantity = rand(1..10)
+    book.dimensions = "h: #{rand(BOOK_DIMENSIONS_RANGE).round(1)} w: #{rand(BOOK_DIMENSIONS_RANGE).round(1)} d: #{rand(BOOK_DIMENSIONS_RANGE).round(1)}"
+    book.year = rand(1850..2019)
+    book.materials = Material.all.sample(rand(1..3))
+    book.category_id = Category.all.sample.id
+    book.authors = Author.all.sample(rand(1..3))
+    book.image = File.open(Rails.root.join("app/assets/images/small/#{rand(1..4)}.jpg"))
+  end
+end

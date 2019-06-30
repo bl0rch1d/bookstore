197b842b6aa88dbe32cc76ab2109696b5ee5ab5c
bl0rch1d <temaflash96@gmail.com>
2019-06-30 20:29:11 +0300

    Added Author, Book, Category, Material models with associations and validations

--- db/migrate/20190630160806_create_join_table_material_book.rb
+++ db/migrate/20190630160806_create_join_table_material_book.rb
@@ -1,13 +1,8 @@
 class CreateJoinTableMaterialBook < ActiveRecord::Migration[5.2]
   def change
     create_join_table :materials, :books do |t|
-<<<<<<< HEAD
       t.index [:material_id, :book_id]
       t.index [:book_id, :material_id]
-=======
-      # t.index [:material_id, :book_id]
-      # t.index [:book_id, :material_id]
->>>>>>> 61d09f106308376e7f2e9fd7c6cafd53f3c6148e
     end
   end
 end

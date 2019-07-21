2cb74af95dacc96b0b426b05cfaf92e05661a3db
bl0rch1d <temaflash96@gmail.com>
2019-07-21 23:12:44 +0300

    Fixed CI

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,26 +1,26 @@
 "use strict";
 
-const MAX_ITEM_QUANTITY = 10
+const MAX_ITEM_QUANTITY = 10;
 
 let toggleDescription = () => {
-    document.getElementById('short_description').classList.toggle("hidden");
-    document.getElementById('full_description').classList.toggle("hidden");
+    document.getElementById("short_description").classList.toggle("hidden");
+    document.getElementById("full_description").classList.toggle("hidden");
 }
 
 let incrementQuantity = () => {
-  let input = document.getElementById('quantity_input');
+  let input = document.getElementById("quantity_input");
 
-  if (input.value < MAX_ITEM_QUANTITY) input.value++;
+  if (input.value < MAX_ITEM_QUANTITY) { input.value++; }
 }
 
 let decrementQuantity = () => {
-  let input = document.getElementById('quantity_input');
+  let input = document.getElementById("quantity_input");
   
-  if (input.value > 1) input.value--;
+  if (input.value > 1) { input.value--; }
 }
 
 document.addEventListener("click", (event) => {
-  let description_triggers = [document.getElementById('full_description_show'), document.getElementById('full_description_hide')]
+  let descriptionTriggers = [document.getElementById("full_description_show"), document.getElementById("full_description_hide")];
   
-  if (description_triggers.includes(event.target)) toggleDescription();
+  if (descriptionTriggers.includes(event.target)) { toggleDescription(); }
 });

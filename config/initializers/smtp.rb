8f645728426d56bfc29cda09a25e54c16185fdaf
bl0rch1d <temaflash96@gmail.com>
2019-09-06 22:17:55 +0300

    Removed redundant code

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,19 +1,3 @@
-# if ENV['MAILTRAP_API_TOKEN']
-#   response = RestClient.get "https://mailtrap.io/api/v1/inboxes.json?api_token=#{ENV['MAILTRAP_API_TOKEN']}"
-
-#   first_inbox = JSON.parse(response)[0]
-
-#   ActionMailer::Base.delivery_method = :smtp
-#   ActionMailer::Base.smtp_settings = {
-#     user_name: first_inbox['username'],
-#     password: first_inbox['password'],
-#     address: first_inbox['domain'],
-#     domain: first_inbox['domain'],
-#     port: first_inbox['smtp_ports'][0],
-#     authentication: :plain
-#   }
-# end
-
 ActionMailer::Base.smtp_settings = {
   domain: 'sample_domain.com',
   address:        "smtp.sendgrid.net",

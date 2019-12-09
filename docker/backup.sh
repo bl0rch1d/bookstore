814605e1010ed2a33a70310c91498827aeebaa2a
bl0rch1d <temaflash96@gmail.com>
2019-12-09 22:55:11 +0200

    Refactor

--- docker/backup.sh
+++ docker/backup.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+retreive_execution_context() {
+    while read LINE; do export "$LINE"; done < /root/cron_ctx
+
+    echo "[+] Execution context retrieved"
+}
+
+dump_db() {
+    pg_dump -h $PG_HOST -p $PG_PORT -U $PG_USERNAME -d $POSTGRES_DATABASE | gzip > dump.sql.gz
+
+    echo "[+] DB dumped"
+}
+
+push_to_sss() {
+    cat dump.sql.gz | aws s3 cp - s3://$S3_BUCKET/$S3_FOLDER/"$POSTGRES_DATABASE"_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz || exit 2
+
+    echo "[+] Pushed to s3"
+}
+
+start() {
+    echo "\n\n[***] Starting backup proccess. $(date +"%Y-%m-%dT%H:%M:%SZ") [***]"
+
+    retreive_execution_context
+    dump_db
+    push_to_sss
+
+    echo "[***] Succeeded! [***]"
+}
+
 set -e
 
-echo 'Retreive execution environment'
-while read LINE; do export "$LINE"; done < cronenv
-
-echo "Start backup proccess..."
-
-pg_dump -h $PG_HOST -p 5432 -U $PG_USERNAME -d $POSTGRES_DATABASE | gzip > dump.sql.gz
-
-echo "Push to s3..."
-
-cat dump.sql.gz | aws s3 cp - s3://$S3_BUCKET/$S3_FOLDER/$POSTGRES_DATABASE_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz || exit 2
-
-echo "Backup pushed successfully!"
+start
--- docker/backup_job.sh
+++ docker/backup_job.sh
@@ -1,39 +1,66 @@
 #!/bin/bash
 
+retreive_aws_credentials() {
+    export CONTAINER_CREDENTIALS=$(curl -s http://$METADATA_ENDPOINT_IP$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI)
+    
+    export AWS_ACCESS_KEY_ID=$(python -c "import sys, os, json; print json.loads(os.environ['CONTAINER_CREDENTIALS'])['AccessKeyId']")
+    
+    export AWS_SECRET_ACCESS_KEY=$(python -c "import sys, os, json; print json.loads(os.environ['CONTAINER_CREDENTIALS'])['SecretAccessKey']")
+    
+    export AWS_SESSION_TOKEN=$(python -c "import sys, os, json; print json.loads(os.environ['CONTAINER_CREDENTIALS'])['Token']")
+
+    unset CONTAINER_CREDENTIALS
+
+    echo "[+] Credentials received"
+}
+
+prepare_execution_context() {
+    echo "PG_HOST=$PG_HOST
+    PG_PORT=$PG_PORT
+    PG_USERNAME=$PG_USERNAME
+    POSTGRES_DATABASE=$POSTGRES_DATABASE
+    S3_BUCKET=$S3_BUCKET
+    S3_FOLDER=$S3_FOLDER
+    S3_REGION=$S3_REGION
+    PGPASSWORD=$PGPASSWORD
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+    AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+    AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN" >> /root/cron_ctx
+
+    echo "[+] Cron execution environment prepared"
+}
+
+prepare_cron_job() {
+    echo "$SCHEDULE /var/www/bookstore/docker/backup.sh >> /var/log/cron.log 2>&1
+    # Extra line for valid cron" > scheduler
+
+    crontab scheduler
+
+    echo "[+] Cron job prepared"
+}
+
+preparations_done_msg() {
+    echo "[*]-------------------------------------------[*]"
+    echo "[*]-------- All preparations done! -----------[*]"
+    echo "[*]-------------------------------------------[*]"
+}
+
+fire_cron() {
+    echo "[***] Firing cron in foreground mode [***]"
+
+    cron -f
+}
+
+start() {
+    echo "DBA container has been started"
+
+    retreive_aws_credentials
+    prepare_execution_context
+    prepare_cron_job
+    preparations_done_msg
+    fire_cron
+}
+
 set -e
 
-echo "DBA container has been started"
-
-echo "Get aws credentials..."
-
-export AWS_ACCESS_KEY_ID=$(curl http://$METADATA_ENDPOINT_IP$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | python -c "import sys, json; print json.load(sys.stdin)['AccessKeyId']")
-
-export AWS_SECRET_ACCESS_KEY=$(curl http://$METADATA_ENDPOINT_IP$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | python -c "import sys, json; print json.load(sys.stdin)['SecretAccessKey']")
-
-export AWS_SESSION_TOKEN=$(curl http://$METADATA_ENDPOINT_IP$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | python -c "import sys, json; print json.load(sys.stdin)['Token']")
-
-echo "Credentials received"
-
-echo "Set cron execution environment"
-env > /root/cronenv
-
-echo "Set background job and start cron"
-
-echo "$SCHEDULE /var/www/bookstore/docker/backup.sh >> /var/log/cron.log 2>&1
-# Extra line for valid cron" > scheduler
-
-
-# echo "$SCHEDULE . $HOME/.profile; /var/www/bookstore/docker/backup.sh $PG_HOST \
-#                                                       $PG_USERNAME \
-#                                                       $POSTGRES_DATABASE \
-#                                                       $S3_BUCKET \
-#                                                       $S3_FOLDER \
-#                                                       $AWS_ACCESS_KEY_ID \
-#                                                       $AWS_SECRET_ACCESS_KEY \
-#                                                       $AWS_SESSION_TOKEN \
-#                                                       $S3_REGION >> /var/log/cron.log 2>&1
-# # Extra line for valid cron" > scheduler
-
-crontab scheduler
-
-cron -f
+start
